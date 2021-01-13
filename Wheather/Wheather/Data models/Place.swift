9e66edca7b0801d9d093d15171cb6324dc1478d4
Anton Sleptsov <anton.sleptsov@gmail.com>
2021-01-13 22:21:38 +0300

    Added basic data structures

---
+++ Wheather/Wheather/Data models/Place.swift
@@ -0,0 +1,16 @@
+//
+//  Place.swift
+//  Wheather
+//
+//  Created by Anton Sleptsov on 13.01.2021.
+//
+
+import Foundation
+
+struct Place {
+    let id: String
+    let name: String
+    
+    let lat: Double
+    let lon: Double
+}
---
+++ Wheather/Wheather/Data models/WeatherSnapsot.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherSnapsot.swift
+//  Wheather
+//
+//  Created by Anton Sleptsov on 13.01.2021.
+//
+
+import Foundation
+
+struct WeatherSnapshot {
+    let temp: Double
+    let tempFeelsLike: Double
+    let wind: Double
+    let humidity: Double
+}
