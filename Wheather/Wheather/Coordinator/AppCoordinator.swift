7d3a466caa571c1a87ee51ef04b31195cfa4db12
Anton Sleptsov <anton.sleptsov@gmail.com>
2021-01-13 17:14:29 +0300

    Added AppCoordinator

---
+++ Wheather/Wheather/Coordinator/AppCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinator.swift
+//  Wheather
+//
+//  Created by Anton Sleptsov on 13.01.2021.
+//
+
+import Foundation
+
+class AppCoordinator {
+    
+    func showOnboarding() {
+        
+    }
+    
+    func showPlacesList() {
+        
+    }
+    
+    func showPlaceDetails() {
+        
+    }
+    
+}
