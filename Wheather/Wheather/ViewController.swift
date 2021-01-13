3209e87613dd48f9edc1a5be41d29cf16e353a0c
Anton Sleptsov <anton.sleptsov@gmail.com>
2021-01-13 15:00:01 +0300

    Initial project creation

---
+++ Wheather/Wheather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wheather
+//
+//  Created by Anton Sleptsov on 13.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
