c4f2e19af46a3438e8ed52caab18bdd8c1540072
Anton Sleptsov <anton.sleptsov@gmail.com>
2021-01-19 11:55:40 +0300

    Added weather mock repository and tests

--- Wheather/Wheather/Data providers/Weather/MockWeatherRepository.swift
+++ Wheather/Wheather/Data providers/Weather/MockWeatherRepository.swift
@@ -12,7 +12,12 @@ class MockWeatherRepository: WeatherRepository {
     func fetchWeather(lat: Double,
                       lon: Double,
                       onFinish: ((WeatherSnapshot?) -> Void)) {
+        let weather = WeatherSnapshot(temp: -10.3,
+                                      tempFeelsLike: -18,
+                                      wind: 0.3,
+                                      humidity: 79)
         
+        onFinish(weather)
     }
     
 }
---
+++ Wheather/WheatherTests/Repositories/MockWeatherRepositoryTests.swift
@@ -0,0 +1,32 @@
+//
+//  MockWeatherRepositoryTests.swift
+//  WheatherTests
+//
+//  Created by Anton Sleptsov on 19.01.2021.
+//
+
+import XCTest
+@testable import Wheather
+
+class MockWeatherRepositoryTests: XCTestCase {
+
+    func testExample() {
+        let sut: WeatherRepository = MockWeatherRepository()
+        
+        let lat = 55.9
+        let lon = 30.8
+        
+        let expectation = XCTestExpectation(description: "Weather data not loaded")
+        
+        sut.fetchWeather(lat: lat,
+                         lon: lon,
+                         onFinish: { weather in
+            XCTAssert(weather != nil, "No weather data provided")
+                            
+            expectation.fulfill()
+        })
+     
+        wait(for: [expectation], timeout: 1)
+    }
+
+}
